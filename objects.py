657aa437c1f005d9627c024934bc044dfdfc2e3b
Dana Geislinger <dg332@nau.edu>
2018-08-13 21:37:14 -0700

    Add functionality to create File objects directly from file path

--- objects.py
+++ objects.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os
+from hashlib import sha1 as func_sha1
 from datetime import datetime
 
 class File():
@@ -9,6 +11,19 @@ class File():
         self.sha1 = sha1
         self.modified = self.parse_datetime(modified)
         self.bin = data
+
+    @classmethod
+    def from_path(cls, path):
+        """ Constructor to create File object from file at path. """
+        name = os.path.basename(path)
+        sha1_obj = func_sha1()
+        with open(path, 'rb') as in_file:
+            data = in_file.read()
+            sha1_obj.update(data)
+        sha1 = sha1_obj.hexdigest()
+        modified = datetime.now()
+        return cls(name, sha1, modified, data)
+                   
 
     def all(self):
         """ Returns tuple of all attributes in above order."""
